22c558f5103039ee5a1d4e4e6dce52a9ba69794a
Matt Link <Linkm1993@gmail.com>
2020-03-11 02:29:09 -0400

    Working directions

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ let byType = ["micro", "regional", "brewpub", "large", "planning", "bar", "contr
 let byTag = ["dog-friendly", "patio", "food-service", "food-trucks", "tours"];
 let states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
 let geo = []
-
 // adding options into the location dropdown
 for (let i = 0; i < states.length; i++){
     let state = $("<option>");
@@ -15,7 +14,6 @@ for (let i = 0; i < states.length; i++){
     state.attr("value", states[i]);
     $("#location").append(state);
 }
-
 // adding options into the type dropdown
 for (let i = 0; i < byType.length; i++){
 	let brewType = $("<option>");
@@ -23,7 +21,6 @@ for (let i = 0; i < byType.length; i++){
 	brewType.attr("value", byType[i]);
 	$("#type").append(brewType);
 }
-
 // adding options into the tag dropdown
 for (let i = 0; i < byTag.length; i++){
 	let brewTag = $("<option>");
@@ -31,7 +28,6 @@ for (let i = 0; i < byTag.length; i++){
 	brewTag.attr("value", byTag[i]);
 	$("#tag").append(brewTag);
 }
-
 //Funcution to get selection from drop down
 function GetSelectedValue(){
 	let tag = $("#tag");
@@ -39,7 +35,6 @@ function GetSelectedValue(){
 	brewTag = tag.val();
 	brewType = type.val();
 }
-
 //Sets search paramerters to location entered by user
 function setQueryUrl(){
 	state = $("#location").val()
@@ -51,7 +46,6 @@ function clear(){
 //On click changes value of state, then makes a ajax call
 $(".btn").on("click", function(event){
 	event.preventDefault();
-
 	//Running the functions
 	clear()
 	setQueryUrl()
@@ -67,7 +61,6 @@ $(".btn").on("click", function(event){
 			"x-rapidapi-key": "8aa2f10370msh3096ef7d1d4d2cdp17f191jsn19e1d73b9115"
 		}
 	}
-	
 	$.ajax(settings).done(function (response) {
 		console.log(response);
 		console.log(state + " " + brewType + " " + brewTag)
@@ -80,17 +73,14 @@ $(".btn").on("click", function(event){
 			let breweryName = response[i].name
 			let brewStreet = response[i].street
 			let brewCity = response[i].city
-
 			// replacing all spaces with a plus sign
 			for (let i = 0; i < brewStreet.length; i++)
 			{
 				brewStreet = brewStreet.replace(" ", "+")
 			}
-
 			let brewState = response[i].state
 			fullAddress = brewStreet + "+" + brewCity + ",+" + brewState;
 			newDiv.attr("value", fullAddress);
-
 			displayDiv.append(newDiv)
 			newDiv.append(breweryName)
 			newDiv.append(brewName)
@@ -99,30 +89,24 @@ $(".btn").on("click", function(event){
 			$(newDiv).addClass("search")
 			newDiv.attr("value", fullAddress)
 		}
-
 		console.log(response)
 		$(".search").on("click", function(){
 			// setting local storage
 			localStorage.setItem("Brewery Location", $(this).attr("value"))
-
 			//opening a new tab
 			window.open(
 				'map.html',
 				'_blank',
 			  );
-			
 			  // mapquest ajax call
 			  mapQuestDirection(geo, fullAddress)
 		})
 	});
-
 })
-
 // user's current location
 function getLocation(){
 	navigator.geolocation.getCurrentPosition(showPosition);
 }
-
 function showPosition(position){
 	lat = position.coords.latitude;
 	long = position.coords.longitude;
@@ -131,51 +115,54 @@ function showPosition(position){
 	geo.push(push)
 	//Storing users geo location in local storage
 	localStorage.setItem("Starting Location", geo[0])
-
 }
 // Setting up map display
 //sets map starting location takes [lat, lon], zoom level
-let mymap = L.map('mapid').setView([39.299236, -76.609383], 13);
-
-// Getting tiles for the map to display
-let tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibGlua20xOTkiLCJhIjoiY2s3anB6ODlxMHlwYjNlbzlkejRlNXhpYiJ9.GJxrvYaqPdUKQazGlKvzfw'
-}).addTo(mymap);
-
+// let mymap = L.map('mapid').setView([39.299236, -76.609383], 13);
+// // Getting tiles for the map to display
+// let tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: 'pk.eyJ1IjoibGlua20xOTkiLCJhIjoiY2s3anB6ODlxMHlwYjNlbzlkejRlNXhpYiJ9.GJxrvYaqPdUKQazGlKvzfw'
+// }).addTo(mymap);
 // getting route for the map to display
-let route = L.
-
-
+// let route = L.directions.route({
+// 	start: userLocation,
+// 	end: address
+// }).addTo(mymap)
 // ajax mapquest request
-function mapQuestDirection(mKey, userLocation, address){
-	let mapQuestURL = "http://www.mapquestapi.com/directions/v2/route?key="+mKey+"&from="+userLocation+"&to="+address
-
-	$.ajax({
-		url: mapQuestURL,
-		method: "GET"
-	}).then(function(response){
-		L.mapquest.directions().route({
-			start: userLocation,
-			end: address,
-		}).addto(mymap);
-		console.log(response);
-	})
-}
-
+// function mapQuestDirection(mKey, userLocation, address){
+// 	let mapQuestURL = "http://www.mapquestapi.com/directions/v2/route?key="+mKey+"&from="+userLocation+"&to="+address
+// 	$.ajax({
+// 		url: mapQuestURL,
+// 		method: "GET"
+// 	}).then(function(response){
+// 		L.mapquest.directions().route({
+// 			start: userLocation,
+// 			end: address,
+// 		}).addto(mymap);
+// 		console.log(response);
+// 	})
+// }
 // getting item from local storage
-let start = localStorage.getItem("Starting Location");
-let end = localStorage.getItem("Brewery Location");
+let startLoc = localStorage.getItem("Starting Location");
+let endLoc = localStorage.getItem("Brewery Location");
 let mqKey = "AE679ItGuD0Kuf8tb45Ir4Koo7Bh2D1L"
-
+L.mapquest.key = "AE679ItGuD0Kuf8tb45Ir4Koo7Bh2D1L"
+let mymap = L.mapquest.map('map', {
+	center: [40.7128, -74.0059],
+	layers: L.mapquest.tileLayer('map'),
+	zoom: 13
+  });
 // getting directions button on click
 $(".directions").on("click", function(event){
 	event.preventDefault();
 
-	//ajax call using the local storage values
-	mapQuestDirection(mqKey, start, end);
-})
+	  L.mapquest.directions().route({
+		start: startLoc,
+		end: endLoc
+	  });
+  });
