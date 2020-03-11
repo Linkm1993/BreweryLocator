5811f540ca384f158753b543141086b002f4dea4
Matt Link <Linkm1993@gmail.com>
2020-03-10 21:48:38 -0400

    Name search returning correct brewery

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ $(".btn").on("click", function(event){
 	clear()
 	setQueryUrl()
 	GetSelectedValue()
-	let queryURL = "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries?by_state=" + state + "&by_tag=" + brewTag + "&by_type=" + brewType;
+	let queryURL = "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries?by_state=" + state + "&by_name="+brewName +"&by_tag=" + brewTag + "&by_type=" + brewType;
 	var settings = {
 		"async": true,
 		"crossDomain": true,
